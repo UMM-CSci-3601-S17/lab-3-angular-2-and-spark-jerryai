cb53e72ced40fd63281acf0c454c8ed18a6fa8e6
chen4709 <chen4709@morris.umn.edu>
2017-02-10 20:39:03 -0600

    Create todos-list.component.html

---
+++ client/src/app/todos/filter.pipe.todos.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by chen4709 on 2/10/17.
+ */
---
+++ client/src/app/todos/todos-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { UserListService } from "./user-list.service";
+import { FormsModule } from '@angular/forms';
+import { FilterBy } from "./filter.pipe";
+
+@Component({
+    selector: 'user-list-component',
+    providers: [UserListService],
+    templateUrl: 'user-list.component.html',
+})
+
+export class UserListComponent {
+    private users: any;
+
+    constructor(private _userListService: UserListService) {
+        this.users = _userListService.getUsers();
+    }
+}
