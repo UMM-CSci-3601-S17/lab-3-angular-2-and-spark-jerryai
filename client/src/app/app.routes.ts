86cc5d9b9e8b3b458f5fc3dc79618e5ea92ca004
chen4709 <chen4709@morris.umn.edu>
2017-02-12 15:50:35 -0600

    Add path to TodosListComponent

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,12 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { KittensComponent } from './kittens/kittens.component';
 import {UserListComponent} from "./users/user-list.component";
+import {TodosListComponent} from "./todos/todos-list.component";
+
 
 // Route Configuration
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'kittens', component: KittensComponent },
-    { path: 'users', component: UserListComponent }
+    { path: 'users', component: UserListComponent },
+    { path: 'todos', component: TodosListComponent }
+
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
