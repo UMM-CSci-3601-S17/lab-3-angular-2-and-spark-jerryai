7a982c6205d4bee8a1abc99a1bccb8cbf5246b64
chen4709 <chen4709@morris.umn.edu>
2017-02-12 15:53:53 -0600

    Create todos-list.service.ts

--- client/src/app/todos/todos-list.service.ts
+++ client/src/app/todos/todos-list.service.ts
@@ -1,3 +1,15 @@
-/**
- * Created by chen4709 on 2/10/17.
- */
+import {Injectable} from '@angular/core';
+import { Http } from '@angular/http';
+import { Todos } from './todos';
+import {Observable} from "rxjs";
+
+@Injectable()
+export class TodosListService {
+    private baseUrl: string = API_URL;
+    constructor(private http:Http) { }
+
+    getTodos(): Observable<Todos[]> {
+        let body = this.http.request(this.baseUrl + 'todos').map(res => res.json());
+        return body;
+    }
+}
